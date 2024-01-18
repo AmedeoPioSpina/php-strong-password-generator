5f81208581bc6b7fd7454fa17efdbb24cd748182
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-18 02:13:43 +0100

    commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+    function passwordGen($count){
+        $password = "";
+        for($i = 0; $i < $count; $i++){
+            $asciiCode = rand(33, 126);
+            $stringConverter = chr($asciiCode);
+            $password .= $stringConverter;
+        };
+        echo $password;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    <form action="index.php" method="GET">
+        <label for="password_length"></label>
+        <input type="text" name="password_length">
+        <button type="submit">Gen</button>
+    </form>
+</body>
+</html>
